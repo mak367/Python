987b94bd242ac746c38214a60375d39bac80edb2
mak367 <85792353+mak367@users.noreply.github.com>
2021-11-12 17:54:29 +0530

    Add files via upload

---
+++ project 1 .py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("STUDENT FORM")
+
+print("enter student name")
+name = str(input())
+
+print ("roll number")
+rollNo= int(input())
+
+print ("marks")
+print("history")
+history=int(input())
+print("maths")
+maths=int(input())
+print("bio")
+bio= int(input())
+print("percent obtained")
+percent=int(input())
+
